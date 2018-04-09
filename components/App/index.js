08691bd15833b4bd32244e4e4d1d68af6506f755
Maxim Ten <maximten94@gmail.com>
2018-04-10 00:08:57 +0700

    bg color transition on scroll added

--- components/App/index.js
+++ components/App/index.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
 import PhotoListContainer from '../../containers/PhotoListContainer'
+import { bgColors, scrollInterval } from '../constants'
 import './index.less'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bgColor: bgColors[0]
+    }
+  }
+  pickColor = (interval, colors, offset) => {
+    const colorIndex = offset % interval / interval * colors.length
+    const startIndex = Math.floor(colorIndex)
+    const endIndex = Math.ceil(colorIndex)
+    const transitionProgress = colorIndex % 1
+    const startColor = colors[startIndex % colors.length]
+    const endColor = colors[endIndex % colors.length]
+    const color = this.getTransitionColor(startColor, endColor, transitionProgress)
+    this.setState({ bgColor: color })
+  }
+  getTransitionColor = (x, y, progress) => {
+    const rgbX = x
+    .replace(/^#/g, '')
+    .match(/.{1,2}/g)
+    .map(item => parseInt(item, 16))
+    const rgbY = y
+    .replace(/^#/g, '')
+    .match(/.{1,2}/g)
+    .map(item => parseInt(item, 16))
+    const vect = [
+      Math.round((rgbY[0] - rgbX[0]) * progress),  
+      Math.round((rgbY[1] - rgbX[1]) * progress),  
+      Math.round((rgbY[2] - rgbX[2]) * progress),  
+    ]
+    const result = '#' + [
+      vect[0] + rgbX[0],
+      vect[1] + rgbX[1],
+      vect[2] + rgbX[2],
+    ]
+    .map(item => item.toString(16))
+    .join('')
+    return result
+  }
+  componentDidMount() {
+    if (typeof window !== 'undefined') {
+      window.onscroll = () => {
+        const offset = window.pageYOffset
+        this.pickColor(scrollInterval, bgColors, offset)
+      } 
+    }
+  }
   render () {
+    const { bgColor } = this.state
     return (
-      <div className="app">
+      <div className="app" style={{ background: bgColor }}>
         <PhotoListContainer/>
       </div>
     )
--- components/constants.js
+++ components/constants.js
@@ -2,3 +2,11 @@ export const screenWidthSm = 480
 export const screenWidthMd = 768
 export const screenWidthBg = 1280
 export const photoWidth = 400
+export const bgColors = [
+  '#247ba0',
+  '#70c1b3',
+  '#b2dbbf',
+  '#f3ffbd',
+  '#ff4476',
+]
+export const scrollInterval = 10000
